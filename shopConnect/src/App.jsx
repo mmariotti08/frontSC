5dcbefa05c4a678f42c597f3cf0efb22f674fb27
mmariotti08 <mariottimatias08@gmail.com>
2023-07-05 18:31:18 -0300

    frecuentQuestions

--- shopConnect/src/App.jsx
+++ shopConnect/src/App.jsx
@@ -4,10 +4,13 @@ import { getProducts } from "./redux/actions";
 import  Login  from "./components/login/Login";
 import Detail from "./views/detail/detail";
 import  Home  from "./views/home/home";
+import AboutUs from "./components/Footer/AboutUS/AboutUs";
+import MeasurSize from "./components/Footer/MeasureSize/MeasureSize";
 import { Footer } from "./components/Footer/Footer";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import { Route, Routes } from "react-router-dom";
+import FrecuentQuestions from "./components/Footer/FrecuentQuestions/FrecuentQuestions";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +26,10 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/products/:id" element={<Detail/>}/>
         <Route  path="/"  element={<Home />}/>
+        {/* RUTAS DEL FOOTER */}
+        <Route path="/fQuestions" element={<FrecuentQuestions/>}/>
+        <Route path="/measureSize" element={<MeasurSize/>}/>
+        <Route path="/aboutUs" element={<AboutUs/>}/>
       </Routes>
       <Footer/>
 
