15ab38780ca9003dc94cbe23735e2c466d807464
mmariotti08 <mariottimatias08@gmail.com>
2023-07-06 17:16:24 -0300

    a desarrollo

--- shopConnect/src/components/Footer/Footer.jsx
+++ shopConnect/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import styles from "./Footer.module.css"
 
 
 const Footer = () => {
     
     return (
-        <div>
+        <div className={styles.container}>
             <div>
                  <h1>Members</h1>
                 <h3>Matias Mariotti</h3>
