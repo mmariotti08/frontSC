f13811072bd426a852a6303b3c5d85e4effcd5f7
joseaot <josealbertoaot@gmail.com>
2023-07-07 14:54:22 -0300

    actual

---
+++ shopConnect/src/components/Order/order.jsx
@@ -0,0 +1,42 @@
+import { useDispatch } from "react-redux";
+import { orderByName, orderByPrice } from "../../redux/actions";
+import style from './Order.module.css'
+
+
+const Order = () => {
+
+    const dispatch = useDispatch();
+
+
+    const handleOrderByName = (event) => {
+        event.preventDefault();
+        dispatch(orderByName(event.target.value));
+      };
+
+      const handleOrderByPrice = (event) => {
+        event.preventDefault();
+        dispatch(orderByPrice(event.target.value));
+      };
+    return(
+        <div className={style.container}>
+            {/* <label className={style.letras}>Order by Name</label> */}
+            <select onChange={(event) => handleOrderByName(event)}className={style.letras}>
+                <option defaultChecked value="0" className={style.letras}> Order by Name</option>
+                <option value="a-z"className={style.letras}>Ascendente</option>
+                <option value="z-a"className={style.letras}>Descendente</option>
+            </select>
+
+            {/* <label className={style.letras}>Order by Price</label> */}
+            <select onChange={(event) => handleOrderByPrice(event)}className={style.letras}>
+                <option defaultChecked value="0" className={style.letras}> Order by Price</option>
+                <option value="asc"className={style.letras}>Precio mas bajo</option>
+                <option value="des" className={style.letras}>Precio mas alto</option>
+            </select>
+
+
+        </div>
+
+    );
+}
+
+export default Order;
