634103b40c85f38a93ce85cd6df52a8804b3dc78
joseaot <josealbertoaot@gmail.com>
2023-07-07 14:49:13 -0300

    Ordenamiento Marye

--- shopConnect/src/redux/reducer.js
+++ shopConnect/src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { GET_DETAIL, GET_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART, REMOVE_FROM_FAV, ADD_TO_FAV, GET_PRODUCT_NAME } from "./actions-type";
+import { GET_DETAIL, GET_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART, REMOVE_FROM_FAV, ADD_TO_FAV, GET_PRODUCT_NAME, ORDER_BY_PRICE,ORDER_BY_NAME } from "./actions-type";
 
 const initialState = {
   products: [],
@@ -44,6 +44,33 @@ const reducer = (state = initialState, action) => {
         ...state,
         fav: state.fav.filter(item => item.id !== action.payload),
       };
+    case ORDER_BY_NAME:
+        const sortedShoes = [...state.products];
+        const sortOrder = action.payload === 'a-z' ? 1 : -1;
+          sortedShoes.sort((shoeA, shoeB) => {
+            if (shoeA.name > shoeB.name) {
+                return 1 * sortOrder;
+            }
+            if (shoeB.name > shoeA.name) {
+                return -1 * sortOrder;
+            }
+            return 0;
+          });
+      return { ...state, products: sortedShoes}
+
+    case ORDER_BY_PRICE:
+        const sortedPrice = [...state.products];
+        const sortOrd = action.payload === 'asc' ? 1 : -1;
+          sortedPrice.sort((priceA, priceB) => {
+            if (priceA.retail_price_cents > priceB.retail_price_cents) {
+                return 1 * sortOrd;
+            }
+            if (priceB.retail_price_cents > priceA.retail_price_cents) {
+                return -1 * sortOrd;
+            }
+            return 0;
+          });
+      return { ...state, products: sortedPrice}
     default:
       return state;
   }
