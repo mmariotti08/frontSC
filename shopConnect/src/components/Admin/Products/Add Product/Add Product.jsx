ab868986823782368c223119f32c16eddb1089dc
MRubilarRiffo <m.rubilar1010@gmail.com>
2023-07-10 20:34:50 -0400

    Fix Form/Add Validation/Change input to select(size)

--- shopConnect/src/components/Admin/Products/Add Product/Add Product.jsx
+++ shopConnect/src/components/Admin/Products/Add Product/Add Product.jsx
@@ -3,9 +3,17 @@ import styles from "./Add Product.module.css";
 import { MdClear, MdAdd } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { createProduct } from "../../../../redux/actions";
+import validation from "./validation";
 
 const Add_Product = () => {
     const dispatch = useDispatch();
+
+    const [error, setError] = useState({});
+
+    const sizes = [
+        "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10",
+        "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14", "14.5", "15"
+    ];
 
     const gender = ["Man", "Woman", "Unisex"];
 
@@ -16,12 +24,10 @@ const Add_Product = () => {
         color: "",
         gender: "Man",
         main_picture_url: "",
-        retail_price_cents: 0,
+        retail_price_cents: "0",
         slug: "",
         status: "",
     });
-
-    console.log(data);
 
     const handleChange = ({ target: { name, value } }) => {
         setData({
@@ -30,40 +36,42 @@ const Add_Product = () => {
         });
     };
 
+    const submit = ({ target: { name, value } }) => {
+        data.slug = `${data.name.replace(/\s/g, "-")}`;
+        const stockIsValid = stocks.every(stock => stock.size !== "" && stock.quantity > 0);
+        const categoryIsValid = data.category.every(category => category !== "");
+        if(!categoryIsValid || !stockIsValid || !data.name.length || !data.brand_name.length || !data.color || !data.main_picture_url || data.retail_price_cents < 0) {
+            setError(validation({ ...data, [name]: value }, stockIsValid, categoryIsValid));
+        } else {
+            dispatch(createProduct(data, stocks));
+            setData({
+                name: "",
+                brand_name: "",
+                category: [""],
+                color: "",
+                gender: "Man",
+                main_picture_url: "",
+                retail_price_cents: 0,
+                slug: "",
+                status: "",
+            });
+            setStocks([{
+                size: "",
+                quantity: 1
+            }]);
+        };
+    };
+
     const handleDraft = (event) => {
         event.preventDefault();
-        data.status = "draft"
-        data.slug = `${data.name.replace(/\s/g, "-")}`
-        dispatch(createProduct(data, stocks));
-        setData({
-            name: "",
-            brand_name: "",
-            category: [""],
-            color: "",
-            gender: "",
-            main_picture_url: "",
-            retail_price_cents: 0,
-            slug: "",
-            status: "",
-        });
+        setData(data.status = "draft");
+        submit(event);
     };
 
     const handleActive = (event) => {
         event.preventDefault();
-        data.status = "active"
-        data.slug = `${data.name.replace(/\s/g, "-")}`
-        dispatch(createProduct(data, stocks));
-        setData({
-            name: "",
-            brand_name: "",
-            category: [""],
-            color: "",
-            gender: "",
-            main_picture_url: "",
-            retail_price_cents: 0,
-            slug: "",
-            status: "",
-        });
+        setData(data.status = "active");
+        submit(event);
     };
 
     // MANEJO DE CATEGORIAS
@@ -75,9 +83,6 @@ const Add_Product = () => {
       };
 
     const handleRemoveCategory = (index) => {
-        if (data.category.length === 1) {
-            return; // Evitar eliminar el último input de categoría
-        }
         setData({
             ...data,
             category: data.category.filter((_, i) => i !== index),
@@ -88,14 +93,13 @@ const Add_Product = () => {
         setData({
             ...data,
             category: data.category.map((category, i) => (i === index ? value : category))
-          });
+        });
     };
-
 
     // MANEJO DE STOCK
     const [stocks, setStocks] = useState([{
         size: "",
-        quantity: 0
+        quantity: "1"
     }]);
 
     const handleAddStock = () => {
@@ -103,15 +107,12 @@ const Add_Product = () => {
             ...stocks,
             {
                 size: "",
-                quantity: 0
+                quantity: "1"
             }
         ]);
     };
 
     const handleRemoveStock = (index) => {
-        if (stocks.length === 1) {
-            return; // Evitar eliminar el último input de stock
-        }
         setStocks(stocks.filter((_, i) => i !== index));
     };
 
@@ -126,7 +127,7 @@ const Add_Product = () => {
             stocks.map((stock, i) => i === index ? { ...stock, quantity: parseInt(value) } : stock)
         );
     };
-
+    console.log(stocks);
     return (
         <div className={styles.container}>
             <div>
@@ -134,17 +135,26 @@ const Add_Product = () => {
             </div>
             <div className={styles.containerform}>
                 <form>
+                    {/* NAME */}
                     <div>
-                        <label htmlFor="name">Product name</label>
+                        <div className={styles.container_label_error}>
+                            <label htmlFor="name">Product name</label>
+                            {error.n1 && <p>{error.n1}</p>}
+                        </div>
+                        <input type="text" name="name" value={data.name} onChange={handleChange} />
                     </div>
-                    <input type="text" name="name" value={data.name} onChange={handleChange} />
+                    {/* BRAND NAME */}
                     <div>
-                        <label htmlFor="brand_name">Brand name</label>
+                        <div className={styles.container_label_error}>
+                            <label htmlFor="brand_name">Brand name</label>
+                            {error.bn1 && <p>{error.bn1}</p>}
+                        </div>
+                        <input type="text" name="brand_name" value={data.brand_name} onChange={handleChange} />
                     </div>
-                    <input type="text" name="brand_name" value={data.brand_name} onChange={handleChange} />
-
+                    {/* CATEGORY */}
                     <div className={styles.category2}>
                         <label htmlFor="category">Category</label>
+                        {error.ca1 && <p>{error.ca1}</p>}
                         <button type="button" onClick={handleAddCategory}><MdAdd /></button>
                     </div>
                     <div className={styles.container_category}>
@@ -166,12 +176,14 @@ const Add_Product = () => {
                             </div>
                         ))}
                     </div>
-
+                    {/* COLOR */}
                     <div>
-                        <label htmlFor="color">Color</label>
+                        <div className={styles.container_label_error}>
+                            <label htmlFor="color">Color</label>
+                            {error.c1 && <p>{error.c1}</p>}
+                        </div>
+                        <input type="text" name="color" value={data.color} onChange={handleChange} />
                     </div>
-                    <input type="text" name="color" value={data.color} onChange={handleChange} />
-                    
                     {/* GENDER */}
                     <div>
                         <label htmlFor="gender">Gender</label>
@@ -179,34 +191,52 @@ const Add_Product = () => {
                     <select name="gender" value={data.gender} onChange={handleChange}>
                         {gender.map((props) => <option key={props} value={props}>{props}</option>)}
                     </select>
-                    {/* <input type="text" name="gender" value={data.gender} onChange={handleChange} /> */}
-                    
+                    {/* IMAGE */}
                     <div>
-                        <label htmlFor="main_picture_url">Image</label>
+                        <div className={styles.container_label_error}>
+                            <label htmlFor="main_picture_url">Image</label>
+                            {error.mp1 && <p>{error.mp1}</p>}
+                        </div>
+                        <input type="text" name="main_picture_url" value={data.main_picture_url} onChange={handleChange} />
                     </div>
-                    <input type="text" name="main_picture_url" value={data.main_picture_url} onChange={handleChange} />
+                    {/* PRICE */}
                     <div>
-                        <label htmlFor="retail_price_cents">Price</label>
+                        <div className={styles.container_label_error}>
+                            <label htmlFor="retail_price_cents">Price</label>
+                            {error.p1 && <p>{error.p1}</p>}
+                        </div>
+                        <input type="number" name="retail_price_cents" value={data.retail_price_cents} onChange={handleChange} min={0} pattern="[0-9]*" />
                     </div>
-                    <input type="number" name="retail_price_cents" value={data.retail_price_cents} onChange={handleChange} pattern="[0-9]*" />
-
-
                     {/* STOCK */}
                     <div className={styles.category2}>
                         <label htmlFor="stock">Stock</label>
+                        {error.s1 && <p>{error.s1}</p>}
                         <button type="button" onClick={handleAddStock}><MdAdd /></button>
                     </div>
                     <div className={styles.container_category}>
                         {stocks.map((stock, index) => (
                             <div key={index} className={styles.category}>
                                 <label htmlFor={`stock-size-${index}`}>Size</label>
-                                <input
+                                <select
+                                    id={`stock-size-${index}`}
+                                    name={`stock-size-${index}`}
+                                    value={stock.size}
+                                    onChange={(event) => handleChangeStockSize(event, index)}
+                                >
+                                    <option value="">Select Size</option>
+                                    {sizes.map((size) => (
+                                        <option key={size} value={size} disabled={stocks.some(stock => stock.size === size)}>
+                                            {size}
+                                        </option>
+                                    ))}
+                                </select>
+                                {/* <input
                                     type="text"
                                     id={`stock-size-${index}`}
                                     name={`stock-size-${index}`}
                                     value={stock.size}
                                     onChange={(event) => handleChangeStockSize(event, index)}
-                                />
+                                /> */}
                                 <label htmlFor={`stock-quantity-${index}`}>Quantity</label>
                                 <input
                                     type="number"
@@ -214,6 +244,8 @@ const Add_Product = () => {
                                     name={`stock-quantity-${index}`}
                                     value={stock.quantity}
                                     onChange={(event) => handleChangeStockQuantity(event, index)}
+                                    min={1}
+                                    pattern="[0-9]*"
                                 />
                                 {stocks.length > 1 && (
                                     <button
@@ -225,10 +257,7 @@ const Add_Product = () => {
                             </div>
                         ))}
                     </div>
-                    {/* AQUI */}
-
-
-
+                    {/* BUTTON */}
                     <div className={styles.container_button}>
                         <button type="submit" onClick={handleDraft}>Save draft</button>
                         <button type="submit" onClick={handleActive}>Publish</button>
--- shopConnect/src/views/admin/Admin.jsx
+++ shopConnect/src/views/admin/Admin.jsx
@@ -2,15 +2,16 @@ import { useSelector } from "react-redux";
 import { Login } from "../../components/Admin/Login/Login";
 import { Menu } from "./Menu";
 import { Routes, Route } from "react-router-dom";
+import styles from "./Admin.module.css"
 
 const Admin = () => {
 
     const getApprovalAdmin = useSelector(state => state.getApprovalAdmin);
 
     return (
-        <>
+        <div className={styles.container_admin}>
             {getApprovalAdmin ? <Menu /> : <Login />}
-        </>
+        </div>
     );
 };
 
