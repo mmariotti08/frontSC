470dca8ce86cec10a86a7a693bbc5f697c901b3f
mmariotti08 <mariottimatias08@gmail.com>
2023-07-12 11:02:54 -0300

    detail

--- shopConnect/src/views/detail/detail.jsx
+++ shopConnect/src/views/detail/detail.jsx
@@ -7,11 +7,14 @@ import { IoCartOutline, IoCartSharp } from "react-icons/io5";
 import { addToCart, removeFromCart, addToFav, removeFromFav } from '../../redux/actions';
 
 
+
 const Detail = () => {
     const dispatch = useDispatch();
     const sneaker = useSelector((state)=>state.detail);
+    const cart = useSelector((state) => state.cart); // Agrega la selección del estado "cart"
+    const fav = useSelector((state) => state.fav);
     const {id} = useParams();
-    console.log(sneaker);
+    
     useEffect(()=>{
         dispatch(getDetail(id))
         window.scrollTo(0, 0);
@@ -22,37 +25,57 @@ const Detail = () => {
         const formattedPrice = (price / 100).toFixed(2);
         return `${formattedPrice}`;
       };
-    const handleCart = ()=>{
-        addToCart(sneaker)
-    }
+
+    const handleCart = () => {
+        if (cart.some(item => item.id === sneaker.id)) {
+            dispatch(removeFromCart(sneaker.id));
+        } else {
+            dispatch(addToCart(sneaker));
+        }
+    };
+    const handleFav = () => {
+        if (fav.some(item => item.id === sneaker.id)) {
+            dispatch(removeFromFav(sneaker.id));
+        } else {
+            dispatch(addToFav(sneaker));
+        }
+    };
+
     return (
         <div className={styles.contDetail}>
             <h1 className={styles.nameh3}>{sneaker.name} </h1>
             <div className={styles.detailz}>
-            <img src={sneaker.main_picture_url} alt="background"/> 
-            <div className={styles.data}>
-            <h2>Brand:</h2>
-            <h3>{sneaker.brand_name}</h3>
-            <h2>Category:</h2>
-            <h3>{sneaker.category}</h3>
-            <h2>Color:</h2>
-            <h3>{sneaker.color}</h3>
-            
-            <h1>$ {formatPrice(sneaker.retail_price_cents)}</h1>
-           
-            <div className={styles.buttons}>
-                
-                    <button onClick={handleCart}>
-                        ADD CART.
-                    </button>
-               
-            
-            </div>
-                
-            </div>
+                <img src={sneaker.main_picture_url} alt="background"/> 
+                <div className={styles.data}>
+                    <h2>Brand:</h2>
+                    <h3>{sneaker.brand_name}</h3>
+                    <h2>Category:</h2>
+                    <h3>{sneaker.category}</h3>
+                    <h2>Color:</h2>
+                    <h3>{sneaker.color}</h3>
+                    
+                    
+                        <h3>SIZES</h3>
+                    <div className={styles.sizes}>
+                    {sneaker.Stocks?.map(s =>(
+                       
+                       s.quantity > 0 ? <button className={styles.size}>{ s.size }</button> : <div className={styles.dess}>{s.size}</div>
+                    ))}
+                    </div>
+                  
+                    <h1>$ {formatPrice(sneaker.retail_price_cents)}</h1>
+                    <div className={styles.buttons}>
+                        <button onClick={handleCart}>
+                            {cart.some(item => item.id === sneaker.id) ? "REMOVE FROM CART" : "ADD TO CART"}
+                        </button>
+                        <button onClick={handleFav}>
+                            {fav.some(item => item.id === sneaker.id) ? "REMOVE FROM FAV" : "ADD TO FAV"}
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Detail
+export default Detail;
