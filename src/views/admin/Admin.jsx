c1bd2fadb27e7524e19fc69f520b06a8df5945dc
MRubilarRiffo <m.rubilar1010@gmail.com>
2023-07-21 17:53:10 -0400

    fix

--- src/views/admin/Admin.jsx
+++ src/views/admin/Admin.jsx
@@ -9,26 +9,8 @@ import { getApproval } from "../../redux/actions";
 
 const Admin = () => {
     const navigate = useNavigate();
-    // const  dispatch = useDispatch();
-
-    // const { isLoaded, user } = useUser();
-
-    // const [adminData, setAdminData] = useState({
-    //     mail: "",
-    //     password: ""
-    // });
 
     const access = useSelector(state => state.getApprovalAdmin);
-
-    // if(isLoaded) {
-    //     console.log(user.primaryEmailAddress.emailAddress);
-    //     adminData.mail = user.primaryEmailAddress.emailAddress
-    //     dispatch(getApproval(adminData));
-    // }
-
-    // useEffect(() => {
-        
-    // }, [isLoaded]);
 
     useEffect(() => {
         !access.access && navigate('/admin');
