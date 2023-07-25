b29f217ed9c1ec67e36ce447ec722b6be39e9c0f
joseaot <josealbertoaot@gmail.com>
2023-07-24 21:22:03 -0300

    borrarlocalstorage

--- src/components/Payments/Successfull/Successfull.jsx
+++ src/components/Payments/Successfull/Successfull.jsx
@@ -1,19 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { useParams } from 'react-router-dom'
-import { fetchOrderData } from '../../../redux/actions'; // Importa la acción definida anteriormente
-import style from './successfull.module.css'
+import { useParams } from 'react-router-dom';
+import { fetchOrderData, removeFromCart } from '../../../redux/actions';
+import style from './successfull.module.css';
 
-const Successfull = ({ orderData, fetchOrderData }) => {
-  const { external_reference } = useParams();       
+const Successfull = ({ orderData, fetchOrderData, removeFromCart }) => {
+  const { external_reference } = useParams();
+  const [showModal, setShowModal] = useState(false); // Estado para controlar la visibilidad del modal
 
   useEffect(() => {
     // Llama a la acción para obtener los datos del endpoint
     fetchOrderData(external_reference);
+
+    // Elimina el carrito del estado global y del LocalStorage
+    clearCart();
   }, [fetchOrderData, external_reference]);
 
+  const clearCart = () => {
+    // Elimina los productos del carrito del estado global
+    orderData.forEach((item) => removeFromCart(item.productId));
+
+    // Elimina el carrito del LocalStorage
+    localStorage.removeItem('cart');
+
+    // Muestra el modal de "compra exitosa"
+    setShowModal(true);
+  };
+  const redirectToHome = () => {
+    // Redirecciona a la página de inicio
+    window.location.href = '/';
+  };
   // Filtra la orden más reciente (la última creada)
   const latestOrder = orderData.length > 0 ? orderData[orderData.length - 1] : null;
+
+
 
   return (
     <div className={style.boton}>
@@ -30,6 +50,15 @@ const Successfull = ({ orderData, fetchOrderData }) => {
       ) : (
         <p>No se encontró ninguna orden para la referencia externa: {external_reference}</p>
       )}
+
+      {/* Modal de "compra exitosa" */}
+      {showModal && (
+        <div className="modal">
+          <h3>¡Compra exitosa!</h3>
+          <p>Tu compra se ha realizado correctamente.</p>
+          <button onClick={redirectToHome}>Ir a Home</button>
+        </div>
+      )}
     </div>
   );
 };
@@ -40,6 +69,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = {
   fetchOrderData,
+  removeFromCart,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Successfull);
