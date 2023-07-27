5da4159673558137bcbeb041c04de43d0236e5f9
Maryeris Orozco <maryeris527@gmail.com>
2023-07-27 08:00:54 -0400

    ultimaActual

--- src/views/profile/profile.jsx
+++ src/views/profile/profile.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import style from './Profile.module.css';
-import { Upload_Image } from '../../components/Admin/Others/Upload Image/Upload Image';
-import { updateOneUser, getUserId } from '../../redux/actions';
+//import { Upload_Image } from '../../components/Admin/Others/Upload Image/Upload Image';
+import { updateOneUser} from '../../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 const Account = () => {
 	const dispatch = useDispatch();
-	const [image, setImage] = useState([]);
+	const [image, setImage] = useState(null);
 	const [editing, setEditing] = useState(false);
 	const [editedUser, setEditedUser] = useState({});
 	
@@ -34,6 +34,16 @@ const Account = () => {
 		}
 	};
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
 	
 	if (!user) {
@@ -42,21 +52,19 @@ const Account = () => {
 
 	return (
 		<div className={style.container}>
-			<a href="/my-account/orders">
-				<button className={style.buttonO}>My Orders</button>
-			</a>
-			<h2 className={style.h2}>My Account</h2>
-			<div className={style}>
-				<Upload_Image image={image} setImage={setImage} />
-			</div>
-			
+					
 		 {editing ? (
 				<div>
+          <h2 className={style.h2}>Edit My Account</h2>
+          <div>
+          {image && <img className={style.image} src={image} alt="Preview" />}
+          <input type="file" accept="image/*" onChange={handleImageChange}
+            />
+          </div>
 			<div>
 				<label className={style.label} htmlFor="name">First Name:</label>
 				<input className={style.input} type="text" id="name" name="name" 
-				value={editedUser.name} onChange={handleInputChange}
-				/>
+				value={editedUser.name} onChange={handleInputChange}/>
 			</div>
 			<div>
 				<label className={style.label} htmlFor="last_name">Last Name:</label>
@@ -83,14 +91,24 @@ const Account = () => {
 			</div>
 				</div>
 			) : (
-				<div>
-
+				<div className={style.contain}>
+          <div>
+          <a href="/my-account/orders">
+				<button className={style.buttonO}>My Orders</button>
+			    </a>
+          </div>
+          <div>
+          <h2 className={style.h2}>My Account</h2>
+          {image && <img src={user.picture} alt="Profile" className={style.image} />}
+          </div>
+          <div>
 					<p className={style.p}>First Name: {user.name}</p>
 					<p className={style.p}>Last Name: {user.last_name}</p>
 					<p className={style.p}>Password: ******** </p>
 					<p className={style.p}>Mail: {user.mail}</p>
 					<p className={style.p}>Phone: {user.phone}</p>
 					<button className={style.button} onClick={handleEditClick}>Editar</button>
+          </div>
 				</div>
 			)} 
 		</div>
